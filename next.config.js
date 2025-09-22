881b44c81fb54a081f08db15b2017b00a162f271
Gatsby01k <vladyslavddos@gmail.com>
2025-09-22 11:49:39 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
-/** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+// next.config.js
+const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true' });
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   poweredByHeader: false,
   swcMinify: true,
-  images: {
-    formats: ["image/avif", "image/webp"],
-    domains: [],
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ["lucide-react"],
+  images: { formats: ["image/avif", "image/webp"], domains: [] },
+  experimental: { optimizeCss: true, optimizePackageImports: ["lucide-react"] },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "bitcoinpetertodd.com" }],
+        destination: "https://www.bitcoinpetertodd.com/:path*",
+        permanent: true,
+      },
+    ];
   },
 });
